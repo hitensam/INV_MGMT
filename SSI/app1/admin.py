c9e1bf64eb99ec3004cab3272e8d02445d5caf6b
Hiten Samalia <30778907+hitensam@users.noreply.github.com>
2023-05-31 11:09:46 +0530

    New Feature Added
    1. Now get CSV File by clicking on download after searching in views/stock/
    2. Fixed settings.py and now DEBUG=False
    3. Modified templates: base.html, view.html, views.py, urls.py (for new feature.)
    4. Cleaned admin.py, IST_TIME.py

--- SSI/app1/admin.py
+++ SSI/app1/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import Sold, stock, customer, user
 # Register your models here.
 
-admin.site.register(Sold);
-admin.site.register(stock);
-admin.site.register(customer);
+admin.site.register(Sold)
+admin.site.register(stock)
+admin.site.register(customer)
 admin.site.register(user)
 
 
--- SSI/app1/urls.py
+++ SSI/app1/urls.py
@@ -6,18 +6,17 @@ from django.views.generic import RedirectView
 app_name = 'app1'
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='view/stock')),
     path("addcustomer/", views.addcustomer, name='addcustomer'),
     path('addstock/', views.addStock, name='addstock'),
     path('view/<str:choice>/', views.view, name = 'view'),
-    # path('view/stock/', views.view, name = 'view'),
     path('addsale/', views.addsale, name = 'sell'),
-    path('', RedirectView.as_view(url='view/stock')),
-    # path('temp/', views.query, name = 'temp')
     # path('logout/', views.logout, name='logout'),
     # path('login/', views.login, name='login'),
     path('search/', views.item_search, name="itemSearch"),
     path('editdata/', views.editData, name='editData'),
-    path('getstock/', views.getfile, name='getStock'),
+    #XXXXXXXXXXXXXXXXXXX UPDATED:ADDED PARAMETERS TO GET CSV BASED ON CHOICE.
+    path('getstock/<str:choice>/<str:value>/', views.getfile, name='getStock'),
      #XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
         #Added 23-05-23
     path('restoredata/', views.restoreData, name='restoreData')
