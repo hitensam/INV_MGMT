b1dcc00aa7ee24aea42f5a67754d9a78570b91d0
Hiten Samalia <30778907+hitensam@users.noreply.github.com>
2023-07-06 15:49:12 +0530

    Delete LaunchDevSrvr.py

--- SSI/LaunchDevSrvr.py
+++
@@ -1,5 +0,0 @@
-import os
-import subprocess
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-subprocess.Popen(["python", "-m", "webbrowser", "-t", "http://localhost:8000/"])
-subprocess.run(["python", "manage.py", "runserver"])
