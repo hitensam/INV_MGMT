6fb5dca2ba6219427206310e482500542d6a0c40
Hiten Samalia <30778907+hitensam@users.noreply.github.com>
2023-07-06 17:23:03 +0530

    SSI -> INV_MGMT

--- INV_MGMT/INV_MGMT/urls.py
+++ INV_MGMT/INV_MGMT/urls.py
@@ -1,4 +1,4 @@
-"""SSI URL Configuration
+"""INV_MGMT URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
